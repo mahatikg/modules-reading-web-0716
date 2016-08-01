038387beefd5537574e2aca58f2a0fa0c8a89bb8
Mahati Gollamudi <github email address>
2016-07-31 22:56:39 -0400

    Alors, en Dance

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,7 @@
+module MetaDancing
+#the module that oversees the classes that dance
+  def metadata
+    "This class produces objects that love to dance."
+  end
+
+end
---
+++ lib/dance_module.rb
@@ -0,0 +1,19 @@
+module Dance
+#unsure how this module is different from the other dance module
+  def twirl
+      "I'm twirling!"
+    end
+
+    def jump
+      "Look how high I'm jumping!"
+    end
+
+    def pirouette
+      "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
+
+end
